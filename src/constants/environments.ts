7bdcb4d650f9e50ed552f1e67940aa0dfb6d930e
luiabdiel <luiggiabdieldev@gmail.com>
2023-11-01 17:02:12 -0300

    feat: updating credentials

--- src/constants/environments.ts
+++ src/constants/environments.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 
+export const HOST = process.env.DATABASE_HOST;
 export const USERNAME = process.env.MYSQL_USERNAME;
 export const PASSWORD = process.env.MYSQL_PASSWORD;
 export const DATABASE = process.env.MYSQL_DATABASE;
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { USERNAME, PASSWORD, DATABASE } from "../constants/environments";
+import { USERNAME, PASSWORD, DATABASE, HOST } from "../constants/environments";
 import { CreateUserTable1698848446762 } from "./migrations/1698848446762-create-user-table";
 import { UserEntity } from "../app/entities/user.entity";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: HOST,
   port: 3306,
   username: USERNAME,
   password: PASSWORD,
