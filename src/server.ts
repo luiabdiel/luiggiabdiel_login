53bac23da185337dba220a80d719a797e945d7b6
luiabdiel <luiggiabdieldev@gmail.com>
2023-11-01 17:04:18 -0300

    feat: add rateLimit

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,23 @@ import cors from "cors";
 import { AppDataSource } from "./database/data-source";
 import { PORT } from "./constants/environments";
 import { routers } from "./app/routes/routes";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
+
+const windowMs = 15 * 60 * 1000; // 15 minutes
+const maxRequestsPerWindow = 100;
+
+const limiter = rateLimit({
+  windowMs,
+  max: maxRequestsPerWindow,
+  message: "Too many requests, please try again later",
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(routers);
+app.use(limiter);
 
 app.get("/", (_, res) => {
   res.send("Hello, Node!");
