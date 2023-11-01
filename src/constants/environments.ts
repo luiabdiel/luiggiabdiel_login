d2e8fa921868c28fb3d8ec9bb5cc736914dc2988
luiabdiel <luiggiabdieldev@gmail.com>
2023-11-01 17:33:59 -0300

    fix

--- src/constants/environments.ts
+++ src/constants/environments.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
 
 export const HOST = process.env.DATABASE_HOST;
-export const USERNAME = process.env.MYSQL_USERNAME;
-export const PASSWORD = process.env.MYSQL_PASSWORD;
-export const DATABASE = process.env.MYSQL_DATABASE;
+export const DBPORT = Number(process.env.DATABASE_PORT);
+export const USERNAME = process.env.DATABASE_NAME;
+export const PASSWORD = process.env.DATABASE_PASSWORD;
+export const DATABASE = process.env.DATABASE_USERNAME;
+
 export const PORT = process.env.PORT;
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,22 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { USERNAME, PASSWORD, DATABASE, HOST } from "../constants/environments";
+import {
+  USERNAME,
+  PASSWORD,
+  DATABASE,
+  HOST,
+  DBPORT,
+} from "../constants/environments";
 import { CreateUserTable1698848446762 } from "./migrations/1698848446762-create-user-table";
 import { UserEntity } from "../app/entities/user.entity";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: HOST,
+  port: DBPORT,
   username: USERNAME,
   password: PASSWORD,
   database: DATABASE,
-  ssl: {
-    rejectUnauthorized: true,
-  },
   synchronize: true,
   logging: false,
   entities: [UserEntity],
